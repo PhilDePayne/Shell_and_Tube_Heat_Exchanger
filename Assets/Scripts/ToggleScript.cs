6e4356665ab01348f6345ecf346687ad8bb3c77f
Markus <markusderiu610@gmail.com>
2022-11-06 12:29:30 +0100

    Link Data Collector and UI

---
+++ Assets/Scripts/ToggleScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleScript : MonoBehaviour
+{
+    [SerializeField] private Toggle toggle;
+    [SerializeField] private DataCollector data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = FindObjectOfType<DataCollector>();
+        toggle.onValueChanged.AddListener((v) => {
+            data.Collect();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
